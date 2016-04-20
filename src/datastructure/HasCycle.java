b51c1b444f3960d651ab9a810c7c1b09d2b24744
Hua Zhang <hua.zhang@wsu.edu>
2016-04-19 21:59:58 -0700

    submit to oj.

--- src/datastructure/HasCycle.java
+++ src/datastructure/HasCycle.java
@@ -9,56 +9,52 @@ public class HasCycle {
 		ListNode head = new ListNode(1);
 		ListNode second = new ListNode(3);
 		ListNode third = new ListNode(1);
-		
-//		head.next = second;
-//		second.next = third;
-		System.out.printf("Test case  %s \n", hasCycle(head) == true ? "success" : "failed");
- 
-	}
 
-	
+		// head.next = second;
+		// second.next = third;
+		System.out.printf("Test case  %s \n", hasCycle(head) == true ? "success" : "failed");
+
+	}
 
 	public static boolean hasCycle(ListNode head) {
 		/*
-		 * 1. Use two pointers : fast and slow
-		 * 2. slow moves step by step,  fast moves two steps at time
-		 * 3. if the Linked List has a cycle fast and slow will meet at some point
-		 *  Ref: https://leetcode.com/discuss/32906/o-1-space-solution
+		 * 1. Use two pointers : fast and slow 2. slow moves step by step, fast
+		 * moves two steps at time 3. if the Linked List has a cycle fast and
+		 * slow will meet at some point Ref:
+		 * https://leetcode.com/discuss/32906/o-1-space-solution
 		 */
-	
-		if(head == null) return false;
-		
+		if (head == null)
+			return false;
+
 		ListNode fast = head;
 		ListNode slow = head;
-		
-		while ( fast.next != null  && fast.next.next != null) {
-			fast = fast.next.next;
+
+		while (fast.next != null && fast.next.next != null) {
 			slow = slow.next;
-			
-			if (fast == slow) { 
+			fast = fast.next.next;
+
+			if (slow == fast) {
 				return true;
 			}
-			
 		}
-		
+
 		return false;
-		
 	}
-	
+
 	public static boolean hasCycle_hashtable(ListNode head) {
 		Hashtable<Integer, Integer> ht = new Hashtable<Integer, Integer>();
 		while (head != null) {
-			if (ht.containsKey(head.val))  {
+			if (ht.containsKey(head.val)) {
 				int count = ht.get(head.val) + 1;
 				ht.put(head.val, count);
 			} else {
 				ht.put(head.val, 1);
 			}
-			head = head.next;	
+			head = head.next;
 		}
-		
+
 		for (int i : ht.values()) {
-			if ( i > 1) {
+			if (i > 1) {
 				return true;
 			}
 		}
